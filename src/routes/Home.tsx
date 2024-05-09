5cf8a382b179961aa4d96b3717825aa4771be9cf
Hira-Yuki <cjsdlf44@gmail.com>
2024-05-09 22:49:26 +0900

    side navigation-bar 생성, 필요 없어진 테스트 코드 삭제

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,17 +1,8 @@
-import { auth } from "../firebase"
 
-/**
- * 
- * protected-route 테스트...
- */
 const Home = () => {
-  const logOut = () =>{
-    auth.signOut()
-    
-  }
+ 
   return (
     <h1>
-      <button onClick={logOut}>log Out</button>
     </h1>
   )
 }
